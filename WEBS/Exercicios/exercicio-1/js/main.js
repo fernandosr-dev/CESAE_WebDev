6067b9ee3a77d7f5ea17a3092651513d52cf12a6
Fernando Rodrigues <fernando.r@email.com>
2024-07-31 12:45:18 +0100

    novos exercicios javascript

---
+++ WEBS/Exercicios/exercicio-1/js/main.js
@@ -0,0 +1,10 @@
+// let myLuckyNumber = 10;
+// let myName = "Fernando";
+
+// alert(`${myName} ${myLuckyNumber}`);
+
+const message =   "   TASTE THE RAINBOW!  "
+
+let whisper = message.trim().toLowerCase();
+
+console.log(whisper);
